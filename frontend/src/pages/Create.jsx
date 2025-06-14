fd53e4878c5f0b59ac8235e05e8c9a5e8aa2a91d
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-06-14 18:23:52 +0530

    Added Create Page to create blog

---
+++ frontend/src/pages/Create.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BlogForm from '../components/BlogForm'
+
+function Create() {
+  return (
+    <div>
+      <h2>Create New Blog</h2>
+      <BlogForm />
+    </div>
+  )
+}
+
+export default Create;
