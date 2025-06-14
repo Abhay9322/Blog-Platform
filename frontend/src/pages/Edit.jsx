ffc52da82deb6721110f7e50e5c7a8d2ad72070f
Abhay Suryawanshi <abhaysuryawanshi9322@gmail.com>
2025-06-14 18:25:55 +0530

    Added Edit page to edit blog by using its id

---
+++ frontend/src/pages/Edit.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import BlogForm from '../components/BlogForm';
+
+function Edit() {
+  return (
+    <div>
+      <h2>Edit Blog</h2>
+      <BlogForm />
+    </div>
+  )
+}
+
+export default Edit;
